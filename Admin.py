c8be70f17d83365eacb80c34b983b7c5bb4c8f0c
khiana <221003.kian@gmail.com>
2025-11-15 00:21:43 +0800

    Refactor Admin.py to include sidebar icons

--- Admin.py
+++ Admin.py
@@ -1,8 +1,8 @@
-import tkinter
 import tkinter as tk
 import tkinter.font as tkFont
+from tkinter import PhotoImage
 
-from PIL import ImageFont
+from PIL import ImageFont, Image, ImageTk
 
 #Admin Window
 root = tk.Tk()
@@ -15,6 +15,11 @@ root.configure(background="#F9FAFB")
 sideBar = tk.Frame(root, bg="#F9D162", width=300, height=700)
 sideBar.place(relx=0, rely=0)
 
+#sidebar bg
+side_path = PhotoImage(file=r"C:\Users\user\Pictures\project\sidebar.png")
+bg_img = tk.Label(sideBar, image=side_path)
+bg_img.place(x=0, y=0)
+
 #font
 font_pth = r"C:\Users\user\OneDrive\Documents\fonts\beachday.ttf"
 try:
@@ -23,57 +28,84 @@ try:
     custom_font = tkFont.Font(family=font_name, size=23, weight="bold")
     pet_font = tkFont.Font(family=font_name, size=30, weight="bold")
 except Exception as e:
-    print("⚠️ Could not load custom font:", e)
-    custom_font = ("Comic Sans MS", 18, "bold")
+    print("Could not load custom font:", e)
+    custom_font = ("Arial", 18, "bold")
+
+def load_icon(path, size=(30,30)):
+    img = Image.open(path)
+    img = img.resize(size, Image.LANCZOS)
+    return ImageTk.PhotoImage(img)
+
+#load all icons
+admin_icon = load_icon(r"C:\Users\user\Pictures\project\user.png")
+pet_icon = load_icon(r"C:\Users\user\Pictures\project\pawprint.png")
+inventory_icon = load_icon(r"C:\Users\user\Pictures\project\file-cabinet.png")
+sales_icon = load_icon(r"C:\Users\user\Pictures\project\sales.png")
+adoption_icon = load_icon(r"C:\Users\user\Pictures\project\share.png")
+staff_icon = load_icon(r"C:\Users\user\Pictures\project\group.png")
+customer_icon = load_icon(r"C:\Users\user\Pictures\project\customer-review.png")
+appointment_icon = load_icon(r"C:\Users\user\Pictures\project\appointment.png")
+report_icon = load_icon(r"C:\Users\user\Pictures\project\report.png")
 
 #logo
+"""
 logo_Label = tk.Label(sideBar, text="CUDDLE CORNER", bg="#F9D162", fg="#2D3436", font=custom_font)
 logo_Label.place(x=40, y=20)
+"""
 
 #section line whatever
 line = tk.Frame(sideBar, bg="#F9FAFB", height=2, width=300)
 line.place(x=0, y=80)
 
-#logIn_btn
-logIn_btn = tk.Button(sideBar, text="Admin", bg="#E67E22", fg="white",
-                      font=("Arial", 12, "bold"), relief="flat", width=20, height=2)
-logIn_btn.place(x=40, y=100)
+#admin_btn
+admin_btn = tk.Button(sideBar, text="Admin", image=admin_icon, compound="left", bg="#F1A842", fg="white",
+                      font=("Arial", 12, "bold"), relief="flat", width=150, height=40, padx=10, anchor="w")
+admin_btn.img = admin_icon
+admin_btn.place(x=40, y=100)
 
 #section line whatever
 line2 = tk.Frame(sideBar, bg="#F9FAFB", height=2, width=300)
 line2.place(x=0, y=170)
 
 #sidebar buttons
-pet_btn = tk.Button(sideBar, text="Pet Management", bg="#F9D162", fg="#2D3436",
-                      font=("Arial", 12, "bold"), relief="flat", width=20, height=2)
+pet_btn = tk.Button(sideBar, text="Pet Management", image=pet_icon, compound="left", bg="#F9D162", fg="#2D3436",
+                      font=("Arial", 12, "bold"), relief="flat", width=180, height=40, padx=10, anchor="w")
+pet_btn.img = pet_icon
 pet_btn.place(x=40, y=180)
 
-inventory_btn = tk.Button(sideBar, text="Inventory Management", bg="#F9D162", fg="#2D3436",
-                          font=("Arial", 12, "bold"), relief="flat", width=20, height=2)
+inventory_btn = tk.Button(sideBar, text="Inventory", image=inventory_icon, compound="left", bg="#F9D162", fg="#2D3436",
+                          font=("Arial", 12, "bold"), relief="flat", width=180, height=40, padx=10, anchor="w")
+inventory_btn.img = inventory_icon
 inventory_btn.place(x=40, y=230)
 
-sales_btn = tk.Button(sideBar, text="Sales Management", bg="#F9D162", fg="#2D3436",
-                      font=("Arial", 12, "bold"), relief="flat", width=20, height=2)
+sales_btn = tk.Button(sideBar, text="Sales Management", image=sales_icon, compound="left", bg="#F9D162", fg="#2D3436",
+                      font=("Arial", 12, "bold"), relief="flat", width=180, height=40, padx=10, anchor="w")
+sales_btn.img = sales_icon
 sales_btn.place(x=40, y=280)
 
-adoption_btn = tk.Button(sideBar, text="Adoption Management", bg="#F9D162", fg="#2D3436",
-                      font=("Arial", 12, "bold"), relief="flat", width=20, height=2)
+adoption_btn = tk.Button(sideBar, text="Adoption", image=adoption_icon, compound="left", bg="#F9D162", fg="#2D3436",
+                      font=("Arial", 12, "bold"), relief="flat", width=180, height=40, padx=10, anchor="w")
+adoption_btn.img = adoption_icon
 adoption_btn.place(x=40, y=330)
 
-staff_btn = tk.Button(sideBar, text="Staff Management", bg="#F9D162", fg="#2D3436",
-                      font=("Arial", 12, "bold"), relief="flat", width=20, height=2)
+staff_btn = tk.Button(sideBar, text="Staff Management", image=staff_icon, compound="left", bg="#F9D162", fg="#2D3436",
+                      font=("Arial", 12, "bold"), relief="flat", width=180, height=40, padx=10, anchor="w")
+staff_btn.img = staff_icon
 staff_btn.place(x=40, y=380)
 
-customer_btn = tk.Button(sideBar, text="Customer Management", bg="#F9D162", fg="#2D3436",
-                      font=("Arial", 12, "bold"), relief="flat", width=20, height=2)
+customer_btn = tk.Button(sideBar, text="Customers", image=customer_icon, compound="left", bg="#F9D162", fg="#2D3436",
+                      font=("Arial", 12, "bold"), relief="flat", width=180, height=40, padx=10, anchor="w")
+customer_btn.img = customer_icon
 customer_btn.place(x=40, y=430)
 
-appointment_btn = tk.Button(sideBar, text="Appointment Scheduling", bg="#F9D162", fg="#2D3436",
-                      font=("Arial", 12, "bold"), relief="flat", width=20, height=2)
+appointment_btn = tk.Button(sideBar, text="Appointments", image=appointment_icon, compound="left", bg="#F9D162", fg="#2D3436",
+                      font=("Arial", 12, "bold"), relief="flat", width=180, height=40, padx=10, anchor="w")
+appointment_btn.img = appointment_icon
 appointment_btn.place(x=40, y=480)
 
-report_btn = tk.Button(sideBar, text="Reports And Analytics", bg="#F9D162", fg="#2D3436",
-                      font=("Arial", 12, "bold"), relief="flat", width=20, height=2)
+report_btn = tk.Button(sideBar, text="Reports Analysis", image=report_icon, compound="left", bg="#F9D162", fg="#2D3436",
+                      font=("Arial", 12, "bold"), relief="flat", width=180, height=40, padx=10, anchor="w")
+report_btn.img = report_icon
 report_btn.place(x=40, y=530)
 
 root.mainloop()
